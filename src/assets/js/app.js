e2c1e8cfdfefcfe2040bafb2c8756ea28e26aaa2
Isaac Morrier <isaacmorrier@gmail.com>
2018-09-27 17:18:36 -0400

    foundation and initial layout

---
+++ src/assets/js/app.js
@@ -0,0 +1,64 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+import libs from './lib/dependancies';
+window.libs = libs;
+
+$(document).foundation();
+
+libs.AOS.init();
+
+// slick carousel
+$(".slideshow").slick({
+  // normal options...
+  speed: 5000,
+	autoplay: true,
+	autoplaySpeed: 0,
+	cssEase: 'linear',
+  slidesToShow: 5,
+	slidesToScroll: 1,
+  infinite: true,
+  swipeToSlide: true,
+	centerMode: true,
+  focusOnSelect: true,
+  // the magic
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+    }, {
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+    }]
+});
+
+// tablesaw table plugin
+$(function () {
+  $(document)
+    .foundation()
+    .trigger('enhance.tablesaw');
+});
+
+var TablesawConfig = {
+  swipeHorizontalThreshold: 15
+};
+
+// app dashboard toggle
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+  e.preventDefault();
+  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
